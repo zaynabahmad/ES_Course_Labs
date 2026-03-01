3fdde34aa1fe554fa45c99ee4534b0c1fa6ce097
ahmed Essam <s-ahmed.emohamed@zewailcity.edu.eg>
2026-03-01 22:58:27 +0200

    Saving MikroC and proteus changes before switching

--- ES_Course_Labs/APP/main.c
+++ ES_Course_Labs/APP/main.c
@@ -5,23 +5,45 @@
 */
 #include "../HAL/LED/LED_interface.h"
 #include "../MCAL/GPIO/GPIO_interface.h"
+#include "../HAL/PUSH_BUTTON/PUSH_BUTTON_INTERFACE.h"
 
-void delay(void)
-{
-    unsigned int i;
-    for(i = 0; i < 5000; i++);
+
+void fast_blinking(){
+     LED_On(GPIO_PORTB, GPIO_PIN0);
+    LED_On(GPIO_PORTB, GPIO_PIN1);
+    Delay_ms(200);
+    LED_Off(GPIO_PORTB, GPIO_PIN0);
+    LED_Off(GPIO_PORTB, GPIO_PIN1);
+    Delay_ms(200);
+}
+
+void slow_blinking(){
+LED_On(GPIO_PORTB, GPIO_PIN0);
+    LED_On(GPIO_PORTB, GPIO_PIN1);
+    Delay_ms(500);
+    LED_Off(GPIO_PORTB, GPIO_PIN0);
+    LED_Off(GPIO_PORTB, GPIO_PIN1);
+    Delay_ms(500);
 }
 
 void main()
 {
     LED_Init(GPIO_PORTB, GPIO_PIN0);
-
+    LED_Init(GPIO_PORTB, GPIO_PIN1);
+    PUSH_BUTTON_Init(GPIO_PORTB, GPIO_PIN2);
+    PUSH_BUTTON_Init(GPIO_PORTB, GPIO_PIN3);
+    
     while(1)
     {
-        LED_On(GPIO_PORTB, GPIO_PIN0);
-        delay();
-
-        LED_Off(GPIO_PORTB, GPIO_PIN0);
-        delay();
+     if (BUTTON_Read(GPIO_PORTB, GPIO_PIN2) == 1) {
+         fast_blinking();
+     }
+     else if (BUTTON_Read(GPIO_PORTB , GPIO_PIN3) == 1) {
+           slow_blinking();
+     }
+     else {
+         LED_Off(GPIO_PORTB, GPIO_PIN0);
+         LED_Off(GPIO_PORTB, GPIO_PIN1);
+     }
     }
 }
