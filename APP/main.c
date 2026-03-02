25df0cc35d56183ac1d7185d9544ab5652e22ef0
YoussefMAllam <s-youssef.allam@zewailcity.edu.eg>
2026-03-03 00:24:49 +0200

    Added Lab task1 in main.c

--- APP/main.c
+++ APP/main.c
@@ -10,9 +10,50 @@
 
 // Inside main.c
 void MyLogic() { LED_Toggle(GPIO_PORTC, GPIO_PIN0); }
-
+void short_sequence() {
+    LED_Toggle(GPIO_PORTC, GPIO_PIN0);
+    LED_Toggle(GPIO_PORTC, GPIO_PIN1);
+    delay_ms(200);
+    LED_Toggle(GPIO_PORTC, GPIO_PIN0);
+    LED_Toggle(GPIO_PORTC, GPIO_PIN1);
+    delay_ms(200);
+}
+void long_sequence() {
+    LED_Toggle(GPIO_PORTC, GPIO_PIN0);
+    LED_Toggle(GPIO_PORTC, GPIO_PIN1);
+    delay_ms(500);
+    LED_Toggle(GPIO_PORTC, GPIO_PIN0);
+    LED_Toggle(GPIO_PORTC, GPIO_PIN1);
+    delay_ms(500);
+}
 void main()
 {
+
+    // Lab Task 1
+    LED_Init(GPIO_PORTC, GPIO_PIN0);
+    LED_Init(GPIO_PORTC, GPIO_PIN1);
+    GPIO_SetPinDirection(GPIO_PORTC, GPIO_PIN0, GPIO_OUTPUT);
+    GPIO_SetPinDirection(GPIO_PORTC, GPIO_PIN1, GPIO_OUTPUT);
+    GPIO_SetPinValue(GPIO_PORTC, GPIO_PIN0, GPIO_LOW);
+    GPIO_SetPinValue(GPIO_PORTC, GPIO_PIN1, GPIO_LOW);
+    SWITCH_Init(GPIO_PORTB, GPIO_PIN0);
+    SWITCH_Init(GPIO_PORTB, GPIO_PIN1);
+
+    while (1)
+    {
+        if (SWITCH_Read(GPIO_PORTB, GPIO_PIN0) == GPIO_HIGH) {
+            short_sequence();
+        }
+        else if (SWITCH_Read(GPIO_PORTB, GPIO_PIN1) == GPIO_HIGH) {
+            long_sequence();
+        }
+        else {
+            GPIO_SetPinValue(GPIO_PORTC, GPIO_PIN0, GPIO_LOW);
+            GPIO_SetPinValue(GPIO_PORTC, GPIO_PIN1, GPIO_LOW);
+        }
+    }
+    
+    // Lab Task 2
     LED_Init(GPIO_PORTC, GPIO_PIN0);
     SWITCH_Init(GPIO_PORTB, GPIO_PIN0);
     EXT_INT_Init();
