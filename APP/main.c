630012c2602297a90e62b32de7feaf22cb3225eb
Abdelhady Mahmoud <153860824+3bdelhadYy@users.noreply.github.com>
2026-03-03 01:16:20 +0200

    Add LED Sequences Control

--- APP/main.c
+++ APP/main.c
@@ -1,13 +1,58 @@
-/*
-* APPLICATION LAYER
-
-
-*/
 #include "LED_interface.h"
 #include "GPIO_interface.h"
 #include "EXT_INT0_interface.h"
 
-/* Callback Function */
+
+// =================>LED Sequences Control<=======================================
+/*
+void delay_ms(unsigned int ms)
+{
+    unsigned int i, j;
+    for(i = 0; i < ms; i++)
+        for(j = 0; j < 1275; j++);  // ~1ms at 8MHz
+}
+
+void main()
+{
+    LED_Init(GPIO_PORTB, GPIO_PIN0);
+    LED_Init(GPIO_PORTB, GPIO_PIN1);
+
+    GPIO_SetPinDirection(GPIO_PORTB, GPIO_PIN2, GPIO_INPUT);
+    GPIO_SetPinDirection(GPIO_PORTB, GPIO_PIN3, GPIO_INPUT);
+
+        while(1)
+    {
+        if(GPIO_GetPinValue(GPIO_PORTB, GPIO_PIN2))  // Switch 1
+        {
+            LED_On(GPIO_PORTB, GPIO_PIN0);
+            LED_On(GPIO_PORTB, GPIO_PIN1);
+            delay_ms(200);
+            LED_Off(GPIO_PORTB, GPIO_PIN0);
+            LED_Off(GPIO_PORTB, GPIO_PIN1);
+            delay_ms(200);
+        }
+        else if(GPIO_GetPinValue(GPIO_PORTB, GPIO_PIN3))  // Switch 2
+        {
+            LED_On(GPIO_PORTB, GPIO_PIN0);
+            LED_On(GPIO_PORTB, GPIO_PIN1);
+            delay_ms(500);
+            LED_Off(GPIO_PORTB, GPIO_PIN0);
+            LED_Off(GPIO_PORTB, GPIO_PIN1);
+            delay_ms(500);
+        }
+        else
+        {
+            LED_Off(GPIO_PORTB, GPIO_PIN0);
+            LED_Off(GPIO_PORTB, GPIO_PIN1);
+        }
+    }
+}
+
+*/
+
+// =======================================>External Interrupt Driver<=======================================
+
+/*
 void App_ToggleLed(void);
 
 void main()
@@ -33,3 +78,5 @@ void App_ToggleLed(void)
 {
     LED_Toggle(GPIO_PORTB, GPIO_PIN1);
 }
+
+*/
