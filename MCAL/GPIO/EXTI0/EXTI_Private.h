e4a83ecf2a2a80eeb39fa87f890b7a50c13ddd2e
Shaban <155167978+Ahmedshaban123r@users.noreply.github.com>
2026-03-04 00:59:42 +0200

    Update EXTI_Private.h

---
+++ MCAL/GPIO/EXTI0/EXTI_Private.h
@@ -0,0 +1,14 @@
+#ifndef EXTI_PRIVATE_H
+#define EXTI_PRIVATE_H
+
+#include <xc.h>
+
+/* INTCON Register Bits */
+#define GIE     7
+#define INTE    4
+#define INTF    1
+
+/* OPTION_REG Bit */
+#define INTEDG  6
+
+#endif
