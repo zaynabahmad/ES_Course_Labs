c6bf3512175a41086b927fab1343f7b178ec1a8f
Shaban <155167978+Ahmedshaban123r@users.noreply.github.com>
2026-03-04 00:40:57 +0200

    Update leds_blinking.c

--- APP/leds_blinking.c
+++ APP/leds_blinking.c
@@ -1,45 +1,62 @@
-#include "LED.h"
-#include "GPIO.h"
+#include "HAL/LED/LED_INTERFACE.h"
+#include "MCAL/GPIO/GPIO_INTERFACE.h"
 #include <util/delay.h>
 
-#define SWITCH1_PORT   PORTD
-#define SWITCH1_PIN    PIN2
+/* LED Definitions */
+#define LED1_PORT     GPIO_PORTB
+#define LED1_PIN      GPIO_PIN0
 
-#define SWITCH2_PORT   PORTD
-#define SWITCH2_PIN    PIN3
+#define LED2_PORT     GPIO_PORTB
+#define LED2_PIN      GPIO_PIN1
 
+/* Switch Definitions */
+#define SWITCH1_PORT  GPIO_PORTD
+#define SWITCH1_PIN   GPIO_PIN2
 
+#define SWITCH2_PORT  GPIO_PORTD
+#define SWITCH2_PIN   GPIO_PIN3
 
 int main(void)
 {
-    LED_Init(LED1);
-    LED_Init(LED2);
+   
+    GPIO_Init();
 
-    GPIO_SetPinDirection(SWITCH1_PORT, SWITCH1_PIN, INPUT);
-    GPIO_SetPinDirection(SWITCH2_PORT, SWITCH2_PIN, INPUT);
+
+    LED_Init(LED1_PORT, LED1_PIN);
+    LED_Init(LED2_PORT, LED2_PIN);
+
+  
+    GPIO_SetPinDirection(SWITCH1_PORT, SWITCH1_PIN, GPIO_INPUT);
+    GPIO_SetPinDirection(SWITCH2_PORT, SWITCH2_PIN, GPIO_INPUT);
+
+ 
+    GPIO_SetPinValue(SWITCH1_PORT, SWITCH1_PIN, GPIO_HIGH);
+    GPIO_SetPinValue(SWITCH2_PORT, SWITCH2_PIN, GPIO_HIGH);
 
     while (1)
     {
-        if (GPIO_ReadPin(SWITCH1_PORT, SWITCH1_PIN) == HIGH)
+        /* Short Sequence (0.2s) */
+        if (GPIO_GetPinValue(SWITCH1_PORT, SWITCH1_PIN) == GPIO_LOW)
         {
-               LED_On(LED1);
-               LED_On(LED2);
-              _delay_ms(200);
+            LED_On(LED1_PORT, LED1_PIN);
+            LED_On(LED2_PORT, LED2_PIN);
+            _delay_ms(200);
 
-              LED_Off(LED1);
-              LED_Off(LED2);
-              _delay_ms(200);
+            LED_Off(LED1_PORT, LED1_PIN);
+            LED_Off(LED2_PORT, LED2_PIN);
+            _delay_ms(200);
         }
-        else if (GPIO_ReadPin(SWITCH2_PORT, SWITCH2_PIN) == HIGH)
+
+        /* Long Sequence (0.5s) */
+        else if (GPIO_GetPinValue(SWITCH2_PORT, SWITCH2_PIN) == GPIO_LOW)
         {
-               LED_On(LED1);
-                LED_On(LED2);
-                _delay_ms(500);
-            
-                LED_Off(LED1);
-                LED_Off(LED2);
-                _delay_ms(500);
+            LED_On(LED1_PORT, LED1_PIN);
+            LED_On(LED2_PORT, LED2_PIN);
+            _delay_ms(500);
+
+            LED_Off(LED1_PORT, LED1_PIN);
+            LED_Off(LED2_PORT, LED2_PIN);
+            _delay_ms(500);
         }
-      
     }
 }
