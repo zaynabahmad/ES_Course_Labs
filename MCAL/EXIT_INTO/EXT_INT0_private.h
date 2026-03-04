8bef87d9dceb64f0dbdce62cd8f2e739945fb1a6
Reemtamer05 <s-reem.elghazawy@zewailcity.edu.eg>
2026-03-04 20:36:59 +0200

    Update EXT_INT0_private.h

--- MCAL/EXIT_INTO/EXT_INT0_private.h
+++ MCAL/EXIT_INTO/EXT_INT0_private.h
@@ -3,16 +3,16 @@
  
  #include "../../SERVICES/STD_TYPES.h"
  
- /* Register definitions for INT0 on PIC16F877A */
+
  #define INTCON      (*((volatile u8*)0x0B))
  #define OPTION_REG  (*((volatile u8*)0x81))
  
  /* INTCON bits */
- #define GIE_BIT     7u   /* Global Interrupt Enable */
- #define INTE_BIT    4u   /* INT0 External Interrupt Enable */
- #define INTF_BIT    1u   /* INT0 External Interrupt Flag */
+ #define GIE_BIT     7u  
+ #define INTE_BIT    4u   
+ #define INTF_BIT    1u   
  
  /* OPTION_REG bits */
- #define INTEDG_BIT  6u   /* Interrupt Edge Select for INT0 */
+ #define INTEDG_BIT  6u  
  
  #endif
