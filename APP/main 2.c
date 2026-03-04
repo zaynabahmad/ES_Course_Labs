3d5b062999eefd8df6d4692f39cd027b0039f214
MalkOsama <s-malk.elnqeesh@zewailcity.edu.eg>
2026-03-04 22:11:32 +0200

    Edited

---
+++ APP/main 2.c
@@ -0,0 +1,44 @@
+void main() {
+
+   TRISB.B1 = 0;   // LED1 output
+   TRISB.B2 = 0;   // LED2 output
+
+   TRISB.B3 = 1;   // Switch1 input
+   TRISB.B4 = 1;   // Switch2 input
+
+   PORTB.B1 = 0;
+   PORTB.B2 = 0;
+
+   while(1) {
+
+      // Short sequence (0.2s)
+      if(PORTB.B3 == 1) {
+
+         PORTB.B1 = 1;
+         PORTB.B2 = 1;
+         Delay_ms(200);
+
+         PORTB.B1 = 0;
+         PORTB.B2 = 0;
+         Delay_ms(200);
+      }
+
+      // Long sequence (0.5s)
+      else if(PORTB.B4 == 1) {
+
+         PORTB.B1 = 1;
+         PORTB.B2 = 1;
+         Delay_ms(500);
+
+         PORTB.B1 = 0;
+         PORTB.B2 = 0;
+         Delay_ms(500);
+      }
+
+      // No switch pressed
+      else {
+         PORTB.B1 = 0;
+         PORTB.B2 = 0;
+      }
+   }
+}
