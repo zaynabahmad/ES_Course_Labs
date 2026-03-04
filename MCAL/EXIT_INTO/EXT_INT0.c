ae80ab2c41096ff0e24bfbca0a038a4dcf61574e
Reemtamer05 <s-reem.elghazawy@zewailcity.edu.eg>
2026-03-04 20:38:05 +0200

    Update EXT_INT0.c

--- MCAL/EXIT_INTO/EXT_INT0.c
+++ MCAL/EXIT_INTO/EXT_INT0.c
@@ -8,31 +8,28 @@
  
  void EXT_INT0_Init(void)
  {
-     /* Configure INT0 pin (RB0) as input */
+   
      GPIO_SetPinDirection(GPIO_PORTB, GPIO_PIN0, GPIO_INPUT);
- 
-     /* Default edge: falling edge (active-low push button with pull-up) */
+
      EXT_INT0_SetEdge(EXT_INT0_FALLING_EDGE);
  
-     /* Clear INT0 flag */
      CLR_BIT(INTCON, INTF_BIT);
  
-     /* Enable INT0 interrupt and global interrupt */
+     
      EXT_INT0_Enable();
  }
  
  void EXT_INT0_Enable(void)
  {
-     /* Enable INT0 external interrupt */
+     
      SET_BIT(INTCON, INTE_BIT);
  
-     /* Enable global interrupts */
      SET_BIT(INTCON, GIE_BIT);
  }
  
  void EXT_INT0_Disable(void)
  {
-     /* Disable INT0 external interrupt */
+     
      CLR_BIT(INTCON, INTE_BIT);
  }
  
@@ -53,19 +50,18 @@
      EXT_INT0_Callback = ptr;
  }
  
- /* ISR implemented in MCAL: calls a callback only (no application logic) */
+ 
  void interrupt(void)
  {
-     /* Check if INT0 flag is set */
+ 
      if(GET_BIT(INTCON, INTF_BIT))
      {
-         /* Call application callback if registered */
+        
          if(EXT_INT0_Callback != NULL_PTR)
          {
              EXT_INT0_Callback();
          }
  
-         /* Clear INT0 flag */
          CLR_BIT(INTCON, INTF_BIT);
      }
  }
