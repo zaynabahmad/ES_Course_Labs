14d45dbd08a9d8ae7c763fdde6fb21268af0a5f5
Reemtamer05 <s-reem.elghazawy@zewailcity.edu.eg>
2026-03-04 23:11:06 +0200

    Update main.c

--- APP/main.c
+++ APP/main.c
@@ -5,16 +5,18 @@
 */
 #include "../HAL/LED/LED_interface.h"
 #include "../HAL/SWITCH/SWITCH_interface.h"
+#include "../MCAL/GPIO/GPIO_interface.h"
 #include "../MCAL/EXT_INT/EXT_INT0_interface.h"
 
 void Toggle_LED_On_Interrupt(void)
 {
-    LED_Toggle(GPIO_PORTB, GPIO_PIN0);
+    LED_Toggle(GPIO_PORTB, GPIO_PIN1);
 }
 
 void main()
 {
-    LED_Init(GPIO_PORTB, GPIO_PIN0);
+    LED_Init(GPIO_PORTB, GPIO_PIN1);
+
     SWITCH_Init(GPIO_PIN0);
 
     EXT_INT0_Init();
