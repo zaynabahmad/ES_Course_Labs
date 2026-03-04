3a0ebfffb42a5546dfa135539d96252a116d0bad
Reemtamer05 <s-reem.elghazawy@zewailcity.edu.eg>
2026-03-04 23:11:57 +0200

    Update SWITCH.c

--- HAL/SWITCH/SWITCH.c
+++ HAL/SWITCH/SWITCH.c
@@ -1,9 +1,7 @@
- #include "SWITCH_interface.h"
- #include "../../MCAL/GPIO/GPIO_interface.h"
- 
- void SWITCH_Init(u8 Pin)
- {
-     /* Configure switch pin on PORTB as input */
-     GPIO_SetPinDirection(GPIO_PORTB, Pin, GPIO_INPUT);
- }
-*** End Patch
+#include "SWITCH_interface.h"
+#include "../../MCAL/GPIO/GPIO_interface.h"
+
+void SWITCH_Init(u8 Pin)
+{
+    GPIO_SetPinDirection(GPIO_PORTB, Pin, GPIO_INPUT);
+}
