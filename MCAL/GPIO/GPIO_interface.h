13ff8a9ef1c24eaef631c08d4954c545e0b9a5e3
Hema-1 <s-ebrahim.khalifa@zewailcity.edu.eg>
2026-03-04 05:56:35 +0200

    Update GPIO_interface.h

--- MCAL/GPIO/GPIO_interface.h
+++ MCAL/GPIO/GPIO_interface.h
@@ -3,14 +3,14 @@
 
 #include "../../SERVICES/STD_TYPES.h"
 
-/* Port IDs */
+
 #define GPIO_PORTA   0
 #define GPIO_PORTB   1
 #define GPIO_PORTC   2
 #define GPIO_PORTD   3
 #define GPIO_PORTE   4
 
-/* Pin IDs */
+
 #define GPIO_PIN0    0
 #define GPIO_PIN1    1
 #define GPIO_PIN2    2
@@ -20,11 +20,11 @@
 #define GPIO_PIN6    6
 #define GPIO_PIN7    7
 
-/* Direction */
+
 #define GPIO_OUTPUT  0
 #define GPIO_INPUT   1
 
-/* Values */
+
 #define GPIO_LOW     0
 #define GPIO_HIGH    1
 
