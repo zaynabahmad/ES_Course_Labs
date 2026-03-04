482f72fe0d7cb7996c953bd6370b677bd4565c79
Reemtamer05 <s-reem.elghazawy@zewailcity.edu.eg>
2026-03-04 23:13:51 +0200

    Update EXT_INT0_private.h

--- MCAL/EXIT_INTO/EXT_INT0_private.h
+++ MCAL/EXIT_INTO/EXT_INT0_private.h
@@ -3,16 +3,15 @@
  
  #include "../../SERVICES/STD_TYPES.h"
  
-
  #define INTCON      (*((volatile u8*)0x0B))
  #define OPTION_REG  (*((volatile u8*)0x81))
  
  /* INTCON bits */
- #define GIE_BIT     7u  
+ #define GIE_BIT     7u   
  #define INTE_BIT    4u   
  #define INTF_BIT    1u   
  
- /* OPTION_REG bits */
- #define INTEDG_BIT  6u  
+#define INTEDG_BIT  6u  
+#define RBPU_BIT    7u   
  
  #endif
