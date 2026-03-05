e6fd69194ea2f4e32a715eaf7aacfa55cd52fd00
Shrooq05 <s-shrooq.saad@zewailcity.edu.eg>
2026-03-05 16:54:33 +0200

    Update EXT_INT0.h

--- task-External Interrupt Driver/MCAL Layer/EXT_INT0.h
+++ task-External Interrupt Driver/MCAL Layer/EXT_INT0.h
@@ -9,7 +9,8 @@ void EXT_INT0_Init(void);
 void EXT_INT0_Enable(void);
 void EXT_INT0_Disable(void);
 void EXT_INT0_SetEdge(uint8 edge_type);
-void EXT_INT0_SetCallback(PtrToFunc ptr);
+void EXT_INT0_SetCallback(void (*ptr)(void));
 
 
 #endif
+
