015c2e85bf96b3f2581164fad0488b2b295ca148
Shrooq05 <s-shrooq.saad@zewailcity.edu.eg>
2026-03-05 15:45:22 +0200

    Add edge definitions for EXT_INT0

--- task-External Interrupt Driver/MCAL Layer/EXT_INT0.h
+++ task-External Interrupt Driver/MCAL Layer/EXT_INT0.h
@@ -1,5 +1,7 @@
                                                                     #ifndef EXT_INT0_H
 #define EXT_INT0_H
+#define RISING_EDGE  1
+#define FALLING_EDGE 0
 
 #include "../Services Layer/Std_Types.h"
 
@@ -8,5 +10,6 @@ void EXT_INT0_Enable(void);
 void EXT_INT0_Disable(void);
 void EXT_INT0_SetEdge(uint8 edge_type);
 void EXT_INT0_SetCallback(PtrToFunc ptr);
+
 
 #endif
