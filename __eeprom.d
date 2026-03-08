7b024a543c8f703aaa8b1f49ea3c2e7dbafcc4d0
Ziad Fahmy <202201027@zewailcity.edu.eg>
2026-03-09 00:59:39 +0200

    Removed extra files

--- __eeprom.d
+++
@@ -1,2 +0,0 @@
-__eeprom.p1: \
-  C:/Program\ Files/Microchip/xc8/v3.10/pic/sources/c99/pic/__eeprom.c
--- main.d
+++
@@ -1,9 +0,0 @@
-main.p1: APP/main.c APP/../HAL/LED/LED_interface.h \
-  APP/../SERVICES/STD_TYPES.h APP/../SERVICES/UTIL.h \
-  APP/../MCAL/GPIO/GPIO_interface.h \
-  APP/../MCAL/GPIO/../../SERVICES/STD_TYPES.h \
-  APP/../MCAL/INT/INT_interface.h SERVICES/STD_TYPES.h \
-  APP/../HAL/SWITCH/SWITCH_interface.h \
-  APP/../HAL/SWITCH/SWITCH_private.h APP/../HAL/SWITCH/SWITCH_config.h \
-  APP/../MCAL/TIMER/TIMER_interface.h APP/../HAL/MOTOR/MOTOR_interface.h \
-  APP/../MCAL/PWM/PWM_interface.h SERVICES/UTIL.h
