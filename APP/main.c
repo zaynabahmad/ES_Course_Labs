9b693e5be31684ff3c58d3aeaf7d5515659d946e
Nada Mohamed <152847465+nadamohamed1230@users.noreply.github.com>
2026-03-10 20:36:28 +0200

    delete comments

--- APP/main.c
+++ APP/main.c
@@ -2,13 +2,10 @@
 #include "lab3.h"
 void main()
 {
-
-
-
     /* LAB 3 - Timer0 Interrupt: Independent LED Toggling */
     lab3_init();
     while(1)
     {
-        /* LEDs are toggled by Timer0 ISR - main loop is free */
+       
     };
 }
