b7053a58f87156825d98a279f7bc1eb3e14d0db7
Amr Mousa <118567286+AmrXAhmed@users.noreply.github.com>
2026-03-12 21:23:05 +0200

    Add files via upload

---
+++ MCAL/GPIO/EXT_INT_interface.c
@@ -0,0 +1,129 @@
+#include "EXT_INT_interface.h"
+
+#include "EXT_INT_private.h"
+
+#include "EXT_INT_config.h"
+
+#include "../GPIO/GPIO_interface.h"
+
+#include "../../SERVICES/BIT_MATH.h"
+
+
+
+void EXT_INT_Init(void)
+
+{
+
+    /* Configure the external interrupt pin as input */
+
+    GPIO_SetPinDirection(EXT_INT_PORT, EXT_INT_PIN, GPIO_INPUT);
+
+
+
+    /* Configure the edge type for the external interrupt */
+
+    EXT_INT_SetEdge(falling_edge); // Default to falling edge, can be changed later
+
+
+
+    /* Clear the interrupt flag */
+
+    CLR_BIT(INTCON, INTF_BIT);
+
+    CLR_BIT(INTCON, INTE_BIT); // Ensure the external interrupt is disabled initially
+
+
+
+}
+
+
+
+void EXT_INT_Enable(void)
+
+{
+
+    /* Enable the external interrupt */
+
+    SET_BIT(INTCON, INTE_BIT);
+
+
+
+    /* Enable global interrupts */
+
+    SET_BIT(INTCON, GIE_BIT);
+
+
+
+    CLR_BIT(INTCON, INTF_BIT); // Clear any pending interrupt flag
+
+}
+
+
+
+void EXT_INT_Disable(void)
+
+{
+
+    /* Clear the interrupt flag */
+
+    CLR_BIT(INTCON, INTF_BIT);
+
+    /* Disable the external interrupt */
+
+    CLR_BIT(INTCON, INTE_BIT);
+
+
+
+}
+
+
+
+void EXT_INT_SetEdge(u8 Edgetype)
+
+{
+
+    if (Edgetype == rising_edge)
+
+    {
+
+        /* Configure for rising edge */
+
+        CLR_BIT(OPTION_REG, INTEDGE_BIT); // INTEDG0 = 0 for rising edge
+
+    }
+
+    else if (Edgetype == falling_edge)
+
+    {
+
+        /* Configure for falling edge */
+
+        SET_BIT(OPTION_REG, INTEDGE_BIT); // INTEDG0 = 1 for falling edge
+
+    }
+
+}
+
+
+
+void (*EXT_INT_Callback)(void) = 0;
+
+
+
+void* Get_EXT_INT_Callback(void) {
+
+    return EXT_INT_Callback;
+
+}
+
+
+
+void EXT_INT_SetCallback(void (*ptr)(void)) {
+
+    if (ptr != 0) {
+
+        EXT_INT_Callback = ptr;
+
+    }
+
+}
---
+++ MCAL/GPIO/EXT_INT_interface.h
@@ -0,0 +1,22 @@
+#ifndef EXT_INT_INTERFACE_H
+#define EXT_INT_INTERFACE_H
+
+#include "../../SERVICES/STD_TYPES.h"
+#include "../GPIO/GPIO_interface.h"
+
+
+/* External Interrupt Configuration */
+
+#define GIE_BIT 7
+#define INTE_BIT 4
+#define INTF_BIT 1
+/* Edge types */
+#define INTEDGE_BIT 6
+#define rising_edge 1
+#define falling_edge 0
+
+void EXT_INT_Init(void);
+void EXT_INT_Enable(void);
+void EXT_INT_Disable(void);
+void EXT_INT_SetEdge(u8 Edgetype);
+#endif
