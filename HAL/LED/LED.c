26f640997157d2ea9123cac99380be6acf9c201f
Ayayasser111 <s-aya.ymohamed@zewailcity.edu.eg>
2026-04-10 01:12:47 +0200

    Fix LED type mismatch

--- HAL/LED/LED.c
+++ HAL/LED/LED.c
@@ -1,24 +1,24 @@
 #include "LED_interface.h"
 #include "../../MCAL/GPIO/GPIO_interface.h"
 
-void LED_Init(char Port, char Pin)
+void LED_Init(u8 Port, u8 Pin)
 {
     GPIO_SetPinDirection(Port, Pin, OUTPUT);
 }
 
-void LED_On(char Port, char Pin)
+void LED_On(u8 Port, u8 Pin)
 {
     GPIO_WritePin(Port, Pin, HIGH);
 }
 
-void LED_Off(char Port, char Pin)
+void LED_Off(u8 Port, u8 Pin)
 {
     GPIO_WritePin(Port, Pin, LOW);
 }
 
-void LED_Toggle(char Port, char Pin)
+void LED_Toggle(u8 Port, u8 Pin)
 {
-    char current = GPIO_ReadPin(Port, Pin);
+    u8 current = GPIO_ReadPin(Port, Pin);
 
     if (current == HIGH)
         GPIO_WritePin(Port, Pin, LOW);
