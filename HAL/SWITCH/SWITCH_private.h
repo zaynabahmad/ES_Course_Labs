87d92f79145485d5f02012b5fd2fdef3086e26d0
Abdelhady Mahmoud <153860824+3bdelhadYy@users.noreply.github.com>
2026-04-10 15:47:03 +0200

    Delete HAL directory

--- HAL/SWITCH/SWITCH_private.h
+++
@@ -1,7 +0,0 @@
-#ifndef SWITCH_PRIVATE_H
-#define SWITCH_PRIVATE_H
-
-#define SW1_ACTIVE_STATE    0  /* SW1 has a pull-up resistor (Active Low) */
-#define SW2_ACTIVE_STATE    1  /* SW2 has a pull-down resistor (Active High) */
-
-#endif
