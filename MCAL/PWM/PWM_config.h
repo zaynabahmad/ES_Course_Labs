4753093f33cfa2557a575a27e0ce088d67932b1d
Abdelhady Mahmoud <153860824+3bdelhadYy@users.noreply.github.com>
2026-04-10 15:47:11 +0200

    Delete MCAL directory

--- MCAL/PWM/PWM_config.h
+++
@@ -1,9 +0,0 @@
-#ifndef PWM_CONFIG_H
-#define PWM_CONFIG_H
-
-/* * Timer2 Period Register Value (Sets PWM Frequency)
- * 255 gives the maximum 10-bit resolution for the duty cycle.
- */
-#define PWM_PR2_VALUE   255
-
-#endif /* PWM_CONFIG_H */
--- MCAL/TIMER_0/TIMER_0_config.h
+++
@@ -1,8 +0,0 @@
-#ifndef TIMER_0_CONFIG_H
-#define TIMER_0_CONFIG_H
-
-#define TIMER0_CLK_SOURCE       TIMER0_INTERNAL_CLK
-#define TIMER0_PRESCALER        TIMER0_PRESCALER_256
-#define TIMER0_PRELOAD_VALUE    61  /* ~50ms per overflow at 4MHz */
-
-#endif
