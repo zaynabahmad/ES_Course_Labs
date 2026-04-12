7809f0e63a3395fea89709535d9b01f787e06dba
Mohammad Maher <101141160+D4NGERX@users.noreply.github.com>
2026-04-12 10:41:47 +0200

    Add files via upload

--- MCAL/ADC/ADC_program.c
+++ MCAL/ADC/ADC_program.c
@@ -1,1 +1,21 @@
+#include "ADC_interface.h"
+#include "ADC_private.h"
+#include "Services/bit_math.h"
 
+void ADC_Init(void)
+{
+    SET_BIT(TRISA_REG, 0);    /* ra0 analog input */
+
+    ADCON1_REG = 0x80;        /* all analog, right-justified, vref = vdd */
+    ADCON0_REG = 0x41;        /* adc on, channel 0, fosc/8 */
+}
+
+unsigned int ADC_Read(unsigned char channel)
+{
+    ADCON0_REG = (ADCON0_REG & CHS_MASK) | (unsigned char)(channel << 3);
+
+    SET_BIT(ADCON0_REG, GO_DONE_BIT);
+    while (GET_BIT(ADCON0_REG, GO_DONE_BIT));
+
+    return (unsigned int)((ADRESH_REG << 8) | ADRESL_REG);
+}
