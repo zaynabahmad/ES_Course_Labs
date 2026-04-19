d5f24a6fd421c2dda66f7010dcd5242d718783cf
Amr Mousa <118567286+AmrXAhmed@users.noreply.github.com>
2026-04-19 17:49:33 +0200

    Add files via upload

---
+++ SERVICES/std_types.h
@@ -0,0 +1,13 @@
+#ifndef STD_TYPES_H
+#define STD_TYPES_H
+
+typedef unsigned char        u8;
+typedef unsigned short       u16;
+typedef unsigned long        u32;
+typedef signed char          s8;
+typedef signed short         s16;
+typedef signed long          s32;
+typedef float                f32;
+
+
+#endif
